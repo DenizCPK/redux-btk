c5b057b7a26b260efd2bf0553b00be6b241896d5
DenizCPK <80528554+DenizCPK@users.noreply.github.com>
2021-03-28 15:23:01 +0300

    Update index.js

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,7 +2,7 @@ import {combineReducers} from "redux"
 import changeCategoryReducer from "./changeCategoryReducer"
 import categoryListReducer from "./categoryListReducer"
 import productListReducer from "./productListReducer"
-import cartReducer from "./categoryListReducer"
+import cartReducer from "./cartReducer"
 
 
 
