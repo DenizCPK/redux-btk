ed2ddb6b0bf6cfa8bf0909a8909d540bc8d9b7a9
DenizCPK <denizcapak@hotmail.com>
2021-03-31 21:54:15 +0300

    addinit

--- src/components/navi/Navi.js
+++ src/components/navi/Navi.js
@@ -23,7 +23,7 @@ const Navi = (props) => {
         <Collapse isOpen={isOpen} navbar>
           <Nav className="ml-auto" navbar>
             <NavItem>
-              <NavLink href="/components/">Ürün EkleD1</NavLink>
+              <NavLink href="/components/">Ürün EkleD12</NavLink>
             </NavItem>
             <CartSummary/>
           </Nav>
